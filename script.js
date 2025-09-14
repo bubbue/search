447f03d133f04891eb638516fa469c1141b07343
dreamnuker-code <mynsq@proton.me>
2025-09-14 22:17:00 +0200

    small change for script.js (detecting blank input)

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ const button = document.getElementById("clicksearch");
 const goBack = document.getElementById("githubSite");
 const usesB = document.getElementById("uses")
 
-sInput.addEventListener("keydown", function(event){
-    textC = event.target.value;
-    if (event.key == "Enter" && textC != "") {
-        window.location.href = "https://www.google.com/search?q=" + textC
-    };
-});
-
-button.addEventListener("click", function(event){
-    textC = sInput.value;
-    if (textC != "") {
+sInput.addEventListener("keydown", function(event) {
+    textC = event.target.value
+    if (event.key == "Enter" && textC.trim() != "") {
         window.location.href = "https://www.google.com/search?q=" + textC
     }
-});
+})
+
+button.addEventListener("click", function(){
+    textC = sInput.value
+    if (textC.trim() != "") {
+        window.location.href = "https://www.google.com/search?q=" + textC
+    }
+})
 
 goBack.addEventListener("click", function(event){
     window.location.href = "https://dreamnuker-code.github.io/"
